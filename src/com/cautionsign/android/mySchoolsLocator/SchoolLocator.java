546f94300301f822c34c06d24bf7219e9a212d49
cautionsign <chan.pen.cheong@gmail.com>
2011-07-04 17:31:03 +0800

    First version of mySchoolsLocator

---
+++ src/com/cautionsign/android/mySchoolsLocator/SchoolLocator.java
@@ -0,0 +1,13 @@
+package com.cautionsign.android.mySchoolsLocator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SchoolLocator extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
